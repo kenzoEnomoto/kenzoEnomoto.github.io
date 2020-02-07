e47a3845bf473cfe0f1aa34184e0801ad681b845
Your Name <you@example.com>
2020-02-07 11:37:19 +0900

    a

--- script/bundle.js
+++ script/bundle.js
@@ -1,4 +1,3 @@
-//
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 
@@ -356,7 +355,7 @@ function _getVisibleTiles() {
             break;
 
           case 14:
-            if (!(visibleTiles.length > 6)) {
+            if (!(visibleTiles.length > 6 || visibleTiles.length == 5)) {
               _context4.next = 23;
               break;
             }
@@ -421,7 +420,6 @@ function _imagesToBlob() {
 
             if (images.length === 6) {
               c.width += images[4].width;
-              c.height += images[5].height;
             }
 
             ctx = c.getContext('2d');
